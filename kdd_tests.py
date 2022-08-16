37137316c56fa16e4266b5e7808b2e6fc892a6f5
Allen <will3670@msu.edu>
2022-08-16 13:04:56 -0400

    commit the files

---
+++ kdd_tests.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 31 00:18:46 2021
+
+@author: Allen
+"""
+
+import pandas as pd
+f = pd.read_csv(r'./NSL-KDD/KDDTrain+_20Percent.txt')
+
+print(f.head())
+
+
+
+# with open('./Datasets/NSL-KDD/KDDTrain+_20Percent') as f:
+    
+    
